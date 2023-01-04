36bef7025abdd1991184a76d87d3320183b315b7
Daehan Lim <penjan.eng@gmail.com>
2023-01-05 01:08:54 +0900

    Prepare call to rule generation and comment errors

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+import urllib.request
+
 import pandas as pd
 from util import util
 from mlxtend.frequent_patterns import apriori
@@ -5,10 +8,10 @@ from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import association_rules
 
 
-def classification_rule_generation(transactions: pd.DataFrame, min_support, corr, min_conf):
+def classification_rule_generation(transactions, min_support, corr, min_conf):
     PCR = pd.DataFrame(columns=['antecedents', 'consequents'])
     NCR = pd.DataFrame(columns=['antecedents', 'consequents'])
-    classes = transactions['class']
+    # classes = transactions['class']
     frequent_itemsets = [pd.DataFrame(columns=['support', 'itemsets'])]
 
     f1 = util.apriori_for_transaction(transactions, min_support=min_support, max_len=1)
@@ -35,29 +38,29 @@ def classification_rule_generation(transactions: pd.DataFrame, min_support, corr
 
 
 
-def ponerg(itemset, classes, corr, min_conf):
-    PCR = pd.DataFrame(columns=['antecedents', 'consequents'])
-    NCR = pd.DataFrame(columns=['antecedents', 'consequents'])
-
-    for c in classes:
-        r = correlation(itemset, c)  # compute correlation between itemset and class c
-        if r > corr:  # generate positive rule
-            # pr = pd.DataFrame({'antecedents': rule_antecedents, 'consequents': rule_consequents})
-            pr = {'antecedent': itemset, 'consequent': {c}}
-            if conf(pr) >= min_conf:
-                PCR.append(pr)
-        elif r < -corr:  # generate negative rules
-            neg_itemset = set()
-            for item in itemset:
-                neg_itemset.add('!' + item)
-            nr1 = {'antecedent': neg_itemset, 'consequent': {c}}
-            nr2 = {'antecedent': itemset, 'consequent': {'!' + c}}
-            if conf(nr1) >= min_conf:
-                NCR.append(nr1)
-            if conf(nr2) >= min_conf:
-                NCR.append(nr2)
-
-    return PCR, NCR
+# def ponerg(itemset, classes, corr, min_conf):
+#     PCR = pd.DataFrame(columns=['antecedents', 'consequents'])
+#     NCR = pd.DataFrame(columns=['antecedents', 'consequents'])
+#
+#     for c in classes:
+#         r = correlation(itemset, c)  # compute correlation between itemset and class c
+#         if r > corr:  # generate positive rule
+#             # pr = pd.DataFrame({'antecedents': rule_antecedents, 'consequents': rule_consequents})
+#             pr = {'antecedent': itemset, 'consequent': {c}}
+#             if conf(pr) >= min_conf:
+#                 PCR.append(pr)
+#         elif r < -corr:  # generate negative rules
+#             neg_itemset = set()
+#             for item in itemset:
+#                 neg_itemset.add('!' + item)
+#             nr1 = {'antecedent': neg_itemset, 'consequent': {c}}
+#             nr2 = {'antecedent': itemset, 'consequent': {'!' + c}}
+#             if conf(nr1) >= min_conf:
+#                 NCR.append(nr1)
+#             if conf(nr2) >= min_conf:
+#                 NCR.append(nr2)
+#
+#     return PCR, NCR
 
 
 
@@ -65,7 +68,19 @@ def ponerg(itemset, classes, corr, min_conf):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # dataset = pd.read_csv('data/store_data.csv', header=None)  # To make sure the first row is not thought of as the heading
+    # # Transforming the list into a list of lists, so that each transaction can be indexed easier
+    # transactions = []
+    # for i in range(0, dataset.shape[0]):
+    #     transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
 
+    records = []
+    for line in urllib.request.urlopen("https://user.informatik.uni-goettingen.de/~sherbold/store_data.csv"):
+        # this also means we need to decode the binary string into ascii
+        records.append(line.decode('ascii').strip().split(','))
+
+    classification_rule_generation(transactions=records, min_support=0.005, min_conf=0.2,
+                                   corr=1)
 
 
     print('PyCharm')
