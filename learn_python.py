53daec8530661f59adfa4bc13cc78eac4ff086b5
Daehan Lim <penjan.eng@gmail.com>
2023-01-08 22:36:48 +0900

    Change learn_python.py into jupyter learn_py.ipynb

--- learn_python.py
+++ learn_python.py
@@ -6,3 +6,12 @@ array2 = array
 array2[0] = 2
 print(array2)
 print(array)
+
+# Create a sample collection
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+
+print(users.copy().items())
+# Strategy:  Iterate over a copy
+for user, status in users.copy().items():
+    if status == 'inactive':
+        del users[user]
