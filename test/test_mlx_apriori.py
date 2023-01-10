e6019d23bf80bfe7bc7947f9b290132c1e6cc945
Daehan Lim <penjan.eng@gmail.com>
2023-01-10 20:23:55 +0900

    test_mlx_apriori.py

--- test/test_mlx_apriori.py
+++ test/test_mlx_apriori.py
@@ -1,4 +1,5 @@
 import csv
+import timeit
 import urllib.request
 import pandas as pd
 from mlxtend.frequent_patterns import apriori
@@ -16,8 +17,9 @@ te = TransactionEncoder()
 te_ary = te.fit_transform(records)
 data_df = pd.DataFrame(te_ary, columns=te.columns_)
 
-frequent_itemsets = apriori(pd.DataFrame(data_df), min_support=0.1, use_colnames=True)
-print(f'{frequent_itemsets}')
+# frequent_itemsets = apriori(pd.DataFrame(data_df), min_support=0.1, use_colnames=True)
+print(timeit.timeit(lambda: apriori(pd.DataFrame(data_df), min_support=0.005, use_colnames=True), number=5))
+# print(f'{frequent_itemsets}')
 
 # rules = association_rules(frequent_itemsets, metric="confidence",
 #                   min_threshold=0.5).sort_values('lift', ascending=False)
