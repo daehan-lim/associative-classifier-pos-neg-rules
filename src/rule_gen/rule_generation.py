298c6c40a332514b0d97d90bd53fdcbf97bf1363
Daehan Lim <penjan.eng@gmail.com>
2023-01-11 19:48:45 +0900

    Temp change rule_generation.py to wrongly pass support_count of ck to posneg

--- src/rule_gen/rule_generation.py
+++ src/rule_gen/rule_generation.py
@@ -12,14 +12,16 @@ def classification_rule_generation(transactions, min_support, corr, min_conf):
 
     # f1 = apriori(pd.DataFrame(transactions_df), min_support=min_support, use_colnames=True, max_len=1)
     c1 = create_candidate_1(transactions)
+    one_itemset_support_count = util.get_support_count(c1, transactions)
     classes = []
     for one_itemset in c1:
         one_itemset_item, = one_itemset
         classes.append(one_itemset_item)
-    f1 = create_freq_itemsets(transactions, c1, min_support=min_support)
+    f1_with_support_count = create_1_freq_itemsets_with_supp(transactions, one_itemset_support_count, min_support=min_support)
+    f1 = f1_with_support_count[0]
     frequent_itemsets = [f1]
-    for item in f1:
-        rules = ponerg(item, classes, corr, min_conf)
+    for item, support_count in zip(*f1_with_support_count):
+        rules = ponerg(item, support_count, classes, one_itemset_support_count, corr, min_conf)
         PCR.extend(rules[0])
         NCR.extend(rules[1])
 
@@ -27,8 +29,10 @@ def classification_rule_generation(transactions, min_support, corr, min_conf):
     k = 0
     while frequent_itemsets[k] is not None and len(frequent_itemsets[k]) > 0:
         ck = _generate_ck_merge(frequent_itemsets[k], f1)
+        ck_support_count = util.get_support_count(ck, transactions)
         for item in ck:
-            rules = ponerg(item, classes, corr, min_conf)
+            rules = ponerg(item, ck_support_count[item], classes,
+                           one_itemset_support_count, corr, min_conf)
             PCR.extend(rules[0])
             NCR.extend(rules[1])
 
@@ -37,6 +41,22 @@ def classification_rule_generation(transactions, min_support, corr, min_conf):
         frequent_itemsets.append(None if k_freq_itemsets.empty
                                  else k_freq_itemsets['itemsets'].tolist())
         k += 1
+    # f1 = apriori(pd.DataFrame(transactions_df), min_support=min_support, use_colnames=True, max_len=1)
+    # frequent_itemsets = [pd.DataFrame(f1)]
+    # k = 1  # change to 1
+    # while len(frequent_itemsets[k - 1]) > 0:
+    #     # itemset_union = _merge_itemsets(frequent_itemsets[k - 1]['itemsets'], f1['itemsets'], k)
+    #     c_k = util.apriori_for_transaction(itemset_union, min_support=min_support, max_len=k + 1)  # F_k-1 U f1
+    #     c_k = util.apriori_of_size_k(pd.DataFrame(transactions_df), min_support=min_support, use_colnames=True, k=k+1)
+    #     frequent_itemset_k = pd.DataFrame(columns=['support', 'itemsets'])
+    #     for index, row in c_k.iterrows():
+    #         if row['support'] >= min_support:
+    #             frequent_itemset_k = frequent_itemset_k.append(row, ignore_index=True)
+    #         # rules = ponerg(itemset, classes, corr, min_conf)
+    #         # PCR = PCR.append(rules[0])
+    #         # NCR = NCR.append(rules[1])
+    #     frequent_itemsets.append(frequent_itemset_k)
+    #     k += 1
 
     return PCR, NCR
     # return PCR, NCR
@@ -82,24 +102,20 @@ def create_candidate_1(transactions):
     return c1
 
 
-def create_freq_itemsets(transactions, ck, min_support):
+def create_1_freq_itemsets_with_supp(transactions, one_itemset_support_count, min_support):
     """
     filters the candidate with the specified minimum support
     """
-    # loop through the transaction and compute the count for each candidate (item)
-    item_support_count = {}
-    for transaction in transactions:
-        for item in ck:
-            if item.issubset(transaction):
-                if item not in item_support_count:
-                    item_support_count[item] = 1
-                else:
-                    item_support_count[item] += 1
+    # one_itemset_support_count: loop through the transaction and compute the count for each candidate (item)
 
     freq_itemsets = []
+    support_count_list = []
     # if the support of an item is greater than the min_support, then it is considered as frequent
-    for item in item_support_count:
-        support = item_support_count[item] / len(transactions)
-        if support >= min_support:
+    for item in one_itemset_support_count:
+        support_count = one_itemset_support_count[item]
+        if support_count / len(transactions) >= min_support:
             freq_itemsets.append(item)
-    return freq_itemsets
+            support_count_list.append(support_count)
+    return freq_itemsets, support_count_list
+
+
--- src/util/util.py
+++ src/util/util.py
@@ -7,3 +7,15 @@ def convert_trans_to_df(transaction):
     te_ary = te.fit_transform(transaction)
     data_df = pd.DataFrame(te_ary, columns=te.columns_)
     return data_df
+
+
+def get_support_count(ck, transactions):
+    item_support_count = {}
+    for transaction in transactions:
+        for item in ck:
+            if item.issubset(transaction):
+                if item not in item_support_count:
+                    item_support_count[item] = 1
+                else:
+                    item_support_count[item] += 1
+    return item_support_count
