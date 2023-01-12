8a27632d684a7a8f1651280c77a0a1ea918748d9
Daehan Lim <penjan.eng@gmail.com>
2023-01-12 12:40:46 +0900

    Consider the rules X -> -C and treat them appropriately

--- src/classification/classification.py
+++ src/classification/classification.py
@@ -9,9 +9,9 @@ def classification(itemset, rules_set, confidence_margin):
         if (rule['antecedent']).issubset(itemset) and rule['consequent'] not in itemset:
             if count == 0:
                 count += 1
-                first_rule_confidence = rule['confidence']
+                first_rule_confidence = abs(rule['confidence'])
                 matching_rules.append(rule)
-            elif rule['confidence'] > first_rule_confidence - confidence_margin:
+            elif abs(rule['confidence']) > first_rule_confidence - confidence_margin:
                 matching_rules.append(rule)
             else:
                 break
@@ -22,40 +22,43 @@ def classification(itemset, rules_set, confidence_margin):
     # Divide the set S into subsets based on category
     rules_by_class = defaultdict(list)
     for rule in matching_rules:
-        rules_by_class[rule['consequent']].append(rule)
+        rules_by_class[rule['consequent'].replace('!', '')].append(rule)
 
     # Calculate the average confidence score for each category
     avg_conf_by_group = dict()
     for rule_group in rules_by_class:
-        avg_conf_by_group[rule_group] = sum(rule['confidence'] for rule in rules_by_class[rule_group]) / len(rules_by_class[rule_group])
+        avg_conf_by_group[rule_group] = sum(rule['confidence'] for rule in rules_by_class[rule_group]) / len(
+            rules_by_class[rule_group])
 
     # Assign the new object to the class with the highest confidence score
-    predicted_class = max(avg_conf_by_group.items(), key=lambda x: x[1])[0]
+    predicted_class = None
+    if (max_confidence := max(avg_conf_by_group.values())) > 0:
+        predicted_class = [c for c, conf in avg_conf_by_group.items() if conf == max_confidence][0]
 
+    # predicted_class = max(avg_conf_by_group.items(), key=lambda x: x[1])[0]
     return predicted_class
 
 
 if __name__ == '__main__':
     my_rules = [{'antecedent': frozenset({'low fat yogurt'}), 'consequent': 'napkins', 'confidence': 0.4},
-             {'antecedent': frozenset({'chocolate'}), 'consequent': 'shrimp', 'confidence': 0.251865671641791},
-             {'antecedent': frozenset({'chicken'}), 'consequent': 'cream', 'confidence': 0.2857142857142857},
-             {'antecedent': frozenset({'red wine'}), 'consequent': 'napkins', 'confidence': 0.2},
-             {'antecedent': frozenset({'low fat yogurt', 'ground beef'}), 'consequent': 'napkins', 'confidence': 0.2},
-             {'antecedent': frozenset({'green tea', 'grated cheese'}), 'consequent': 'napkins', 'confidence': 0.2},
-             {'antecedent': frozenset({'frozen vegetables', 'grated cheese'}), 'consequent': 'napkins',
-              'confidence': 0.2},
-             {'antecedent': frozenset({'spaghetti', 'grated cheese'}), 'consequent': 'napkins', 'confidence': 0.2},
-             {'antecedent': frozenset({'ground beef', 'spaghetti'}), 'consequent': 'cream',
-              'confidence': 0.2857142857142857},
-             {'antecedent': frozenset({'spaghetti', 'herb & pepper'}), 'consequent': 'napkins', 'confidence': 0.2},
-             {'antecedent': frozenset({'pancakes', 'grated cheese'}), 'consequent': 'napkins', 'confidence': 0.2},
-             {'antecedent': frozenset({'pancakes', 'butter'}), 'consequent': 'napkins', 'confidence': 0.2},
-             {'antecedent': frozenset({'ground beef', 'grated cheese'}), 'consequent': 'napkins', 'confidence': 0.2},
-             {'antecedent': frozenset({'grated cheese', 'butter'}), 'consequent': 'napkins', 'confidence': 0.2}]
+                {'antecedent': frozenset({'chocolate'}), 'consequent': 'shrimp', 'confidence': 0.251865671641791},
+                {'antecedent': frozenset({'chicken'}), 'consequent': 'cream', 'confidence': 0.2857142857142857},
+                {'antecedent': frozenset({'red wine'}), 'consequent': 'napkins', 'confidence': 0.2},
+                {'antecedent': frozenset({'low fat yogurt', 'ground beef'}), 'consequent': 'napkins',
+                 'confidence': 0.2},
+                {'antecedent': frozenset({'green tea', 'grated cheese'}), 'consequent': 'napkins', 'confidence': 0.2},
+                {'antecedent': frozenset({'frozen vegetables', 'grated cheese'}), 'consequent': 'napkins',
+                 'confidence': 0.2},
+                {'antecedent': frozenset({'spaghetti', 'grated cheese'}), 'consequent': 'napkins', 'confidence': 0.2},
+                {'antecedent': frozenset({'ground beef', 'spaghetti'}), 'consequent': 'cream',
+                 'confidence': 0.2857142857142857},
+                {'antecedent': frozenset({'spaghetti', 'herb & pepper'}), 'consequent': 'napkins', 'confidence': 0.2},
+                {'antecedent': frozenset({'pancakes', 'grated cheese'}), 'consequent': 'napkins', 'confidence': 0.2},
+                {'antecedent': frozenset({'pancakes', 'butter'}), 'consequent': 'napkins', 'confidence': 0.2},
+                {'antecedent': frozenset({'ground beef', 'grated cheese'}), 'consequent': 'napkins', 'confidence': 0.2},
+                {'antecedent': frozenset({'grated cheese', 'butter'}), 'consequent': 'napkins', 'confidence': 0.2}]
     my_itemset = frozenset(['napkins', 'grated cheese', 'butter'])
     my_confidence_margin = 0.001
-
-    print('napkins' not in my_itemset)
 
     # matching_rules = []
     # count = 0
@@ -71,17 +74,28 @@ if __name__ == '__main__':
     #         else:
     #             break
 
-    # # rules_by_class = defaultdict(list)
-    # #
-    # # for rule in my_rules:
-    # #     rules_by_class[rule['consequent']].append(rule)
-    # #
-    # # # Calculate the average confidence score for each category
-    # # scores = dict()
-    # # for rule_group in rules_by_class:
-    # #     scores[rule_group] = sum(rule['confidence'] for rule in rules_by_class[rule_group]) / len(
-    # #         rules_by_class[rule_group])
+    # rules_by_class = defaultdict(list)
+    #
+    # matching_rules = [
+    #     {'antecedent': frozenset({'chocolate'}), 'consequent': '!shrimp', 'confidence': 0.251865671641791},
+    #     {'antecedent': frozenset({'almond'}), 'consequent': 'shrimp', 'confidence': 0.351865671641791},
+    #     {'antecedent': frozenset({'chicken'}), 'consequent': '!shrimp', 'confidence': -0.2857142857142857}
+    # ]
+    # for rule in matching_rules:
+    #     rules_by_class[rule['consequent'].replace('!', '')].append(rule)
+    #
+    # # Calculate the average confidence score for each category
+    # scores = dict()
+    # for rule_group in rules_by_class:
+    #     # all('!' in rule['consequent'] for rule in rules_by_class[rule_group])
+    #     scores[rule_group] = sum(rule['confidence'] for rule in rules_by_class[rule_group]) / len(
+    #         rules_by_class[rule_group])
+    #
+    # if (max_score := max(scores.values())) > 0:
+    #     predicted_class = [c for c, score in scores.items() if score == max_score][0]
+    # else:
+    #     predicted_class = None
     #
     # # Assign the new object to the class with the highest confidence score
-    # predicted_class = max(scores.items(), key=lambda x: x[1])[0]
-
+    # # predicted_class = max(scores.items(), key=lambda x: x[1])[0]
+    # predicted_class
