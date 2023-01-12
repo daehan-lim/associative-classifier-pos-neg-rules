7dc314de22549bb744c06118e66ec4efaf27cf26
Daehan Lim <penjan.eng@gmail.com>
2023-01-12 11:40:50 +0900

    Sort rules before passing them to classification

--- src/main.py
+++ src/main.py
@@ -20,8 +20,10 @@ if __name__ == '__main__':
 
     PCR, NCR = rule_generation.classification_rule_generation(transactions=records,
                                                               min_support=0.02, min_conf=0.05, corr=0.05)
+
     itemset = frozenset(['!mineral water', 'shrimp'])
-    predicted_class = classification.classification(itemset, PCR + NCR, 0.001)
+    sorted_rules = sorted(PCR + NCR, key=lambda d: abs(d['confidence']), reverse=True)
+    predicted_class = classification.classification(itemset, sorted_rules, 0.001)
     pr = np.expand_dims(np.array(PCR), axis=1)
     nr = np.expand_dims(np.array(NCR), axis=1)
     print(PCR)
