f6cc89b5d1bb542476b3f46c4e180ad61e03e990
Daehan Lim <penjan.eng@gmail.com>
2023-01-19 04:34:38 +0900

    Progressively slide correlation threshold until a class is predicted

--- src/main.py
+++ src/main.py
@@ -18,12 +18,28 @@ if __name__ == '__main__':
         for row in csv.reader(file):
             records.append(list(filter(None, row)))
 
-    PCR, NCR = rule_generation.classification_rule_generation(transactions=records,
-                                                              min_support=0.05, min_conf=0.06, corr=0.05)
+    PCR = []
+    NCR = []
+    predicted_class = None
+    itemset = frozenset(['2 ML  -  METOCLOPRAMIDE HCL 5 MG/ML IJ SOLN',
 
-    itemset = frozenset(['METOPROLOL SUCCINATE ER 50 MG PO TB24', '2 ML  -  METOCLOPRAMIDE HCL 5 MG/ML IJ SOLN'])
-    sorted_rules = sorted(PCR + NCR, key=lambda d: abs(d['confidence']), reverse=True)
-    predicted_class = classification.classification(itemset, sorted_rules, 0.1)
+                         ])
+    corr = 0.5
+    while corr > 0.001:
+        PCR, NCR = rule_generation.classification_rule_generation(
+            transactions=records, min_support=0.05, min_conf=0.2, corr=corr)
+        if len(PCR + NCR) > 0:
+            sorted_rules = sorted(PCR + NCR, key=lambda d: abs(d['confidence']), reverse=True)
+            predicted_class = classification.classification(itemset, sorted_rules, 0.1)
+            if predicted_class is not None:
+                break
+            corr -= 0.01
+            continue
+        if corr > 0.11:
+            corr -= 0.1
+        else:
+            corr -= 0.03
+
     pr = np.expand_dims(np.array(PCR), axis=1)
     nr = np.expand_dims(np.array(NCR), axis=1)
     print(PCR)
