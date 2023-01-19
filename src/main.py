3ecfa11b32f4ca39ac97dd419ceabdb76db24db7
Daehan Lim <penjan.eng@gmail.com>
2023-01-19 12:48:15 +0900

    Test algorithm with different support and conf to classify as 'Alive'

--- src/main.py
+++ src/main.py
@@ -21,24 +21,22 @@ if __name__ == '__main__':
     PCR = []
     NCR = []
     predicted_class = None
-    itemset = frozenset(['2 ML  -  METOCLOPRAMIDE HCL 5 MG/ML IJ SOLN',
+    itemset = frozenset(['ACETAMINOPHEN 325 MG PO TABS',
 
                          ])
     corr = 0.5
     while corr > 0.001:
         PCR, NCR = rule_generation.classification_rule_generation(
-            transactions=records, min_support=0.05, min_conf=0.08, corr=0.02)
+            transactions=records, min_support=0.1, min_conf=0.09, corr=0.04)
         if len(PCR + NCR) > 0:
             sorted_rules = sorted(PCR + NCR, key=lambda d: abs(d['confidence']), reverse=True)
             predicted_class = classification.classification(itemset, sorted_rules, 0.1)
             if predicted_class is not None:
                 break
-            corr -= 0.01
-            continue
         if corr > 0.11:
             corr -= 0.1
         else:
-            corr -= 0.03
+            corr -= 0.04
 
     pr = np.expand_dims(np.array(PCR), axis=1)
     nr = np.expand_dims(np.array(NCR), axis=1)
