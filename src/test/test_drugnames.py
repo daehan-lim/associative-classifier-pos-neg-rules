48de52679cea0be8ea5bc46992a06d89172aa973
Daehan Lim <penjan.eng@gmail.com>
2023-01-19 13:25:36 +0900

    Change test_drugnames to generate final_table_all_attr with 0, 1 in db

--- src/test/test_drugnames.py
+++ src/test/test_drugnames.py
@@ -9,5 +9,5 @@ drug_names = np.array(records).flatten().tolist()
 result = ''
 for drug_name in drug_names:
     drug_name = drug_name.replace("'", "''")
-    result = f"{result}CASE WHEN MAX(CASE WHEN drugname = '{drug_name}' THEN 1 ELSE 0 END) = 1 THEN '{drug_name}' END as \"{drug_name[0:60]}\",\n"
+    result = f"{result}MAX(CASE WHEN drugname = '{drug_name}' THEN 1 ELSE 0 END) as \"{drug_name[0:60]}\",\n"
 result
