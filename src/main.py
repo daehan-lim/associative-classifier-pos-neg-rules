cf730c191b4cdd00fd00d519e8edc4ecc2dbd53d
Daehan Lim <penjan.eng@gmail.com>
2023-01-27 16:42:13 +0900

    Add test_dataset.csv and use  it to test classification

--- src/main.py
+++ src/main.py
@@ -7,22 +7,32 @@ from src.rule_gen import rule_generation
 if __name__ == '__main__':
 
     with open('../data/training_dataset.csv', 'r') as file:
-        records = [list(filter(None, row)) for row in csv.reader(file)]
+        training_set = [list(filter(None, row)) for row in csv.reader(file)]
 
-    object_o = frozenset(['ACETAMINOPHEN 325 MG PO TABS',
-                          ])
+    with open('../data/test_dataset.csv', 'r') as file:
+        test_set = [list(filter(None, row)) for row in csv.reader(file)]
+    first_transaction = test_set[0].copy()
+    first_transaction.pop()
+    first_object = frozenset([item for item in first_transaction])
+
     PCR, NCR = rule_generation.classification_rule_generation(
-        transactions=records, classes=[frozenset(['1']), frozenset(['0'])], min_support=0.05,
+        transactions=training_set, classes=[frozenset(['1']), frozenset(['0'])], min_support=0.05,
         min_conf=0.05, corr=0.03)
     sorted_rules = sorted(PCR + NCR, key=lambda d: abs(d['confidence']), reverse=True)
-    predicted_class = classification.classification(object_o, sorted_rules, 0.1)
+
+    real_classes = []
+    predicted_classes = []
+    for transaction in test_set:
+        real_classes.append(transaction[-1])
+        object_o = frozenset([item for item in transaction[:-1]])
+        predicted_classes.append(classification.classification(object_o, sorted_rules, 0.1))
 
     pr = np.expand_dims(np.array(PCR), axis=1)
     nr = np.expand_dims(np.array(NCR), axis=1)
-    print(PCR)
+    print(pr)
     print(NCR)
-    print(f"Object to classify: {object_o}")
-    print(f"Predicted class: {predicted_class}")
+    print(f"Object to classify: {first_object}")
+    print(f"Predicted class: {predicted_classes[0]}")
 
     # print(rule_generation.classification_rule_generation(transactions=records,
     #                                                      min_support=0.05, min_conf=0.2, corr=0.3))
