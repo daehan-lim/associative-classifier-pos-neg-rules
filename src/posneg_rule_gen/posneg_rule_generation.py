eeaf2a0b02c56c7cdc31d58f4fecfbd5c4a17070
Daehan Lim <penjan.eng@gmail.com>
2023-02-02 17:07:37 +0900

    Fix bug in conf calculation
    Now divide by antecedent support instead of consequent support

--- src/posneg_rule_gen/posneg_rule_generation.py
+++ src/posneg_rule_gen/posneg_rule_generation.py
@@ -9,27 +9,27 @@ def ponerg(itemset, classes, class_support_count_dict, corr, min_conf, transacti
 
     for c in classes:
         combined_support_count = util.get_item_support_count_df(itemset | c, transactions_df)
-        r = correlation(itemset, c, transactions_df, combined_support_count, class_support_count_dict[c])
+        antecedent_support_count = util.get_item_support_count_df(itemset, transactions_df)
+        r = correlation(itemset, c, transactions_df, antecedent_support_count, combined_support_count, class_support_count_dict[c])
         c_str, = c
         if r > corr:  # generate positive rule
             # pr = pd.DataFrame({'antecedents': rule_antecedents, 'consequents': rule_consequents})
-            if (conf := confidence(combined_support_count, class_support_count_dict[c])) >= min_conf:
+            if (conf := confidence(combined_support_count, antecedent_support_count)) >= min_conf:
                 PCR.append({'antecedent': itemset, 'consequent': c_str, 'confidence': conf, 'correlation': r})
         elif r < -corr:  # generate negative rules
             combined_support_count_nr1 = util.get_support_count_not_i_and_c(itemset, c_str, transactions_df)
             combined_support_count_nr2 = util.get_support_count_i_and_not_c(itemset, c_str, transactions_df)  # fix
-            if (conf := confidence(combined_support_count_nr1, class_support_count_dict[c])) >= min_conf:
+            not_i_support_count = util.get_item_support_count_df(itemset, transactions_df, negated=True)
+            if (conf := confidence(combined_support_count_nr1, not_i_support_count)) >= min_conf:
                 neg_itemset = frozenset(['!' + item for item in itemset])
                 NCR.append({'antecedent': neg_itemset, 'consequent': c_str, 'confidence': conf, 'correlation': r})
-            if (conf := confidence(combined_support_count_nr2, util.get_item_support_count_df(
-                    c, transactions_df, negated=True))) >= min_conf:
+            if (conf := confidence(combined_support_count_nr2, antecedent_support_count)) >= min_conf:
                 NCR.append({'antecedent': itemset, 'consequent': '!' + c_str, 'confidence': -conf, 'correlation': r})
 
     return PCR, NCR
 
 
-def correlation(itemset, c, transactions_df, f11, f_plus_1):
-    f1_plus = util.get_item_support_count_df(itemset, transactions_df)
+def correlation(itemset, c, transactions_df, f1_plus, f11, f_plus_1):
     f00 = util.get_item_support_count_df(itemset | c, transactions_df, negated=True)
 
     f01 = f_plus_1 - f11
@@ -45,13 +45,6 @@ def correlation(itemset, c, transactions_df, f11, f_plus_1):
     #     [f_plus_1, f_plus_0, len(transactions_df.index)],
     # ]
 
-    # with warnings.catch_warnings():
-    #     warnings.filterwarnings('error')
-    #     try:
-    #         corr = (f11 * f00 - f10 * f01) / math.sqrt(f_plus_0 * f_plus_1 * f1_plus * f0_plus)
-    #     except Warning as e:
-    #         a = 2
-    #         b = 3
     if f_plus_0 * f_plus_1 * f1_plus * f0_plus == 0:
         return 0
     return (f11 * f00 - f10 * f01) / math.sqrt(f_plus_0 * f_plus_1 * f1_plus * f0_plus)
@@ -67,5 +60,5 @@ def correlation(itemset, c, transactions_df, f11, f_plus_1):
 # f0+ = f01 + f00
 
 
-def confidence(combined_support_count, consequent_support_count):
-    return combined_support_count / consequent_support_count
+def confidence(combined_support_count, antecedent_support_count):
+    return combined_support_count / antecedent_support_count
