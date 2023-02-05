Fix print: k, confusion matrix, precision and recall added

src/main.py
@@ -1,6 +1,7 @@
 import csv
 import timeit
 import numpy as np
+from tabulate import tabulate
 
 from classification import classification
 from rule_gen import rule_generation
@@ -24,9 +25,7 @@ if __name__ == '__main__':
         min_conf=min_conf)
     sorted_rules = sorted(rules, key=lambda d: abs(d['confidence']), reverse=True)
 
-    # print(timeit.timeit(lambda: rule_generation.classification_rule_generation(
-    #     transactions=training_set, m_classes=[frozenset(['1']), frozenset(['0'])], m_min_support=0.03,
-    #     m_min_conf=min_conf), number=1))
+    # print(timeit.timeit(lambda: rule_generation.classification_rule_generation(), number=1))
 
     real_classes = []
     predicted_classes = []
@@ -38,15 +37,44 @@ if __name__ == '__main__':
     accuracy = 100 * np.sum(y_true == y_pred) / len(y_true)
 
     print(f"min_support = {min_support},  min_conf = {min_conf}")
-    print(f"# of classes predicted as '0': {np.count_nonzero(y_pred == 0)}  "
-          f"out of {np.count_nonzero(y_true == 0)} in real set")
-    print(f"# of classes predicted as '1': {np.count_nonzero(y_pred == 1)}  "
-          f"out of {np.count_nonzero(y_true == 1)} in real set")
-    # print(f"Correctly Predicted as 0: {np.sum(y_true == 0 and y_pred == 0)}")
-    # print(f"Correctly Predicted as 1: {np.sum(y_true == 1 and y_pred == 1)}")
-    print(f"# of misclassifications: {np.count_nonzero(y_pred == -1)}")
+
+    TP = np.sum(np.logical_and(y_pred == 1, y_true == 1))
+    TN = np.sum(np.logical_and(y_pred == 0, y_true == 0))
+    #Predicted a label of 1 (Alive), but the true label is 0.
+    FP = np.sum(np.logical_and(y_pred == 1, y_true == 0))
+    # Predicted a label of 0 (Dead), but the true label is 1.
+    FN = np.sum(np.logical_and(y_pred == 0, y_true == 1))
+    #Predicted as -1 when actual class = 1 (positive)
+    NO_P = np.sum(np.logical_and(y_pred == -1, y_true == 1))
+    # Predicted as -1 when actual class = 0
+    NO_N = np.sum(np.logical_and(y_pred == -1, y_true == 0))
+
+    confusion_matrix = [
+        ["1=died  0=alive", "Pred class = '1'", "Pred class = '0'", "Pred c = '-1'", 'Total actual c'],
+        ["Actual class = '1'", str(TP) + "\n(TP)", str(FN) + "\n(FN)",
+         str(NO_P) + "\n(Pred as -1, actual = 1)", str(TP + FN + NO_P) + "\n(Total actual c= '1')"],
+        ["Actual class = '0'", str(FP) + "\n(FP)", str(TN) + "\n(TN)",
+         str(NO_N) + "\n(Pred as -1, actual = 0)", str(FP + TN + NO_N) + "\n(Total actual c= '0')"],
+        ["Total pred c",
+         str(TP + FP) + "\n(Total pred as '1')",
+         str(FN + TN) + "\n(Total pred as '0')",
+         str(NO_P + NO_N) + "\n(Total pred as '-1')", str(len(test_set))],
+    ]
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    print(tabulate(confusion_matrix, headers='firstrow', tablefmt='fancy_grid'))
+    print(f"Max length of freq itemsets (k): {len(rules[-1]['antecedent']) - 1}")
+    # print(f"# of classes predicted as '0': {np.count_nonzero(y_pred == 0)}  "
+    #       f"out of {np.count_nonzero(y_true == 0)} in real set")
+    # print(f"# of classes predicted as '1': {np.count_nonzero(y_pred == 1)}  "
+    #       f"out of {np.count_nonzero(y_true == 1)} in real set")
+    # print(f"# of classes predicted as '-1': {np.count_nonzero(y_pred == -1)}")
+    # print(f"Correctly Predicted as 0: {np.sum(np.logical_and(y_pred == 0, y_true == 0))}")
+    # print(f"Correctly Predicted as 1: {np.sum(np.logical_and(y_pred == 1, y_true == 1))}")
     print(f"Rules: {len(rules)}")
     print(f"Accuracy: {accuracy}%")
+    print(f"Precision: {round(precision, 3)}")
+    print(f"Recall: {round(recall, 3)}")
 
     # pr = np.expand_dims(np.array(PCR), axis=1)
     # print(timeit.timeit(lambda: rule_generation.classification_rule_generation(