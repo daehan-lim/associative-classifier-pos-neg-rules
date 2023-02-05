b60347ca73df018f7d8c54631614636510aa8153
Daehan Lim <penjan.eng@gmail.com>
2023-02-05 16:42:24 +0900

    Revert "Test difference between attributes in training and test sets (Temp)"

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 import csv
 import timeit
-import pandas as pd
 import numpy as np
-from mlxtend.preprocessing import TransactionEncoder
 
 from classification import classification
 from rule_gen import rule_generation
@@ -16,23 +14,6 @@ if __name__ == '__main__':
 
     with open('../data/test_dataset.csv', 'r') as file:
         test_set = [list(filter(None, row)) for row in csv.reader(file)]
-
-    te = TransactionEncoder()
-    te_ary = te.fit_transform(training_set)
-    transactions_df = pd.DataFrame(te_ary, columns=te.columns_)
-    te.columns_.remove('1')  # generalize later without using class names
-    te.columns_.remove('0')
-
-
-    te2 = TransactionEncoder()
-    te_ary2 = te2.fit_transform(test_set)
-    transactions_df2 = pd.DataFrame(te_ary2, columns=te2.columns_)
-    te2.columns_.remove('1')  # generalize later without using class names
-    te2.columns_.remove('0')
-
-    diff = set(te.columns_).symmetric_difference(set(te2.columns_))
-    a_not_b = set(te.columns_) - set(te2.columns_)
-    b_not_a = set(te2.columns_) - set(te.columns_)
 
     min_support = 0.04
     min_conf = 0.05
