ac41c0b4b671752917325622ea43709ae47bce35
Daehan Lim <penjan.eng@gmail.com>
2023-02-14 00:28:31 +0900

    Label unclassified instances as 0 (majority class)

--- src/classification/classification.py
+++ src/classification/classification.py
@@ -5,5 +5,5 @@ def classification(object_o, rules_set, confidence_margin):
     for rule in rules_set:
         if (rule['antecedent']).issubset(object_o):
             return int(rule['consequent'])
-    return -1
+    return 0
 
