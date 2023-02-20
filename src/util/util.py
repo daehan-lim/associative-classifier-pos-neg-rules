f72366d6a8b4372780ba6a814c6e432cba1b9bd1
Daehan Lim <penjan.eng@gmail.com>
2023-02-20 15:13:43 +0900

    Refactor util.py

--- src/util/util.py
+++ src/util/util.py
@@ -9,33 +9,6 @@ def convert_trans_to_df(transaction):
     return data_df
 
 
-def get_support_count_dict_df(ck, transactions_df):
-    item_support_count = {}
-    for item in ck:
-        item_support_count[item] = get_item_support_count_df(item, transactions_df)
-    return item_support_count
-
-
-def get_support_count_dict(ck, transactions):
-    item_support_count = {}
-    for transaction in transactions:
-        for item in ck:
-            if item.issubset(transaction):
-                if item not in item_support_count:
-                    item_support_count[item] = 1
-                else:
-                    item_support_count[item] += 1
-    return item_support_count
-
-
-def get_item_support_count(item, transactions):
-    support_count = 0
-    for transaction in transactions:
-        if item.issubset(transaction):
-            support_count += 1
-    return support_count
-
-
 def get_item_support_count_df(itemset: frozenset, df, negated=False):
     """
     Efficient support calculation
@@ -72,3 +45,33 @@ def get_support_count_i_and_not_c(itemset: frozenset, class_str, df):
     support = (temp & ~df[class_str]).sum()
 
     return support
+
+
+'''
+def get_support_count_dict_df(ck, transactions_df):
+    item_support_count = {}
+    for item in ck:
+        item_support_count[item] = get_item_support_count_df(item, transactions_df)
+    return item_support_count
+
+
+def get_support_count_dict(ck, transactions):
+    item_support_count = {}
+    for transaction in transactions:
+        for item in ck:
+            if item.issubset(transaction):
+                if item not in item_support_count:
+                    item_support_count[item] = 1
+                else:
+                    item_support_count[item] += 1
+    return item_support_count
+
+
+
+def get_item_support_count(item, transactions):
+    support_count = 0
+    for transaction in transactions:
+        if item.issubset(transaction):
+            support_count += 1
+    return support_count
+'''
