2d9dc125f0e4f7e72d3b90e01108874c950e8e10
Daehan Lim <penjan.eng@gmail.com>
2023-03-15 23:44:43 +0900

    Change main like prof_main, show correct auc, show f1 with 6 decimal places
    Split training and test sets in code with 9:1
    Get rid of conf (selection)
    Use new lift instead

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import random
+import pandas as pd
 from sklearn.metrics import classification_report, roc_auc_score
 import csv
 import numpy as np
@@ -5,33 +7,52 @@ from tabulate import tabulate
 from classification import classification
 from rule_gen import rule_generation
 from daehan_mlutil import utilities
+from util import util
 
 
 @utilities.timeit
 def main():
-    with open('../data/training_set_big_h.csv', 'r') as file:
-        training_set = [list(filter(None, row)) for row in csv.reader(file)]
+    with open('../data/dataset.csv', 'r') as file:
+        dataset = [list(filter(None, row)) for row in csv.reader(file)]
 
-    with open('../data/test_set_big_h.csv', 'r') as file:
-        test_set = [list(filter(None, row)) for row in csv.reader(file)]
+    # with open('../data/test_set_big_h.csv', 'r') as file:
+    #     test_set = [list(filter(None, row)) for row in csv.reader(file)]
 
     # avg_transaction_size = sum(len(transaction) for transaction in training_set) / len(training_set)
     # min_transaction_size = min(len(transaction) for transaction in training_set)
     # max_transaction_size = max(len(transaction) for transaction in training_set)
 
-    min_support = 0.07
-    min_conf = 0.07
-    corr = 0.001
-    print(f"supp = {min_support},  conf = {min_conf}, \n")
+    min_support = 0.05
+    print(f"supp = {min_support} \n")
+    transactions_df = util.convert_trans_to_df(dataset)
+
+    random.seed(0)
+    transactions_0 = pd.DataFrame(
+        transactions_df[transactions_df['0']].reset_index(drop=True))
+    transactions_1 = pd.DataFrame(
+        transactions_df[transactions_df['1']].reset_index(drop=True))
+
+    indices = list(range(0, len(transactions_0)))
+    random.shuffle(indices)
+    test_set_0 = transactions_0.iloc[indices[:417], :].reset_index(drop=True)
+    training_set_0 = transactions_0.iloc[indices[417:], :].reset_index(drop=True)
+
+    indices = list(range(0, len(transactions_1)))
+    random.shuffle(indices)
+    test_set_1 = transactions_1.iloc[indices[:43], :].reset_index(drop=True)
+    training_set_1 = transactions_1.iloc[indices[43:], :].reset_index(drop=True)
+
+    training_set = pd.concat([training_set_0, training_set_1])
+    test_set = pd.concat([test_set_0, test_set_1])
 
     rules, freq_itemsets = rule_generation.classification_rule_generation(transactions=training_set,
-                                                           m_min_support=min_support, m_min_conf=min_conf)
+                                                                          m_min_support=min_support)
     rules_0 = [rule for rule in rules if rule['consequent'] == '0']
     rules_1 = [rule for rule in rules if rule['consequent'] == '1']
 
     sorted_rules = sorted(rules, key=lambda d: d['confidence'], reverse=True)
 
-    y_test, y_pred, not_classified = predict(test_set, sorted_rules)
+    y_test, y_pred, scores, not_classified = predict(test_set, sorted_rules)
 
     TP = np.sum(np.logical_and(y_pred == 1, y_test == 1))
     TN = np.sum(np.logical_and(y_pred == 0, y_test == 0))
@@ -56,8 +77,8 @@ def main():
     print(f"Pred as -1: {not_classified}")
     print(f"Precision: {round(precision, 3)}")
     print(f"Recall: {round(recall, 3)}")
-    print(f"F1 (harmonic mean): {round(F1, 3)}")
-    print('roc auc: %.3f' % roc_auc_score(y_test, y_pred))
+    print(f"F1: {round(F1, 6)}")
+    print('roc auc: %.6f' % roc_auc_score(y_test, scores))
     print(f"Accuracy: {round(accuracy, 3)}%")
     print(f"Total Rules: {len(sorted_rules)}")
     print(f"Rules with class 0: {len(rules_0)}")
@@ -92,16 +113,15 @@ def main():
 
 @utilities.timeit
 def predict(test_set, sorted_rules):
-    y_test = []
-    y_pred = []
-    for transaction in test_set:
-        y_test.append(int(transaction[-1]))
-        object_o = frozenset([item for item in transaction[:-1]])
-        y_pred.append(classification.classification(object_o, sorted_rules, 0.1))
+    y_test = test_set.apply(lambda row: 0 if row['0'] else 1, axis=1).tolist()
+    objects = test_set.drop(['1', '0'], axis=1).apply(lambda row: frozenset(row.index[row]), axis=1).tolist()
+    predictions = [classification.classify(object_o, sorted_rules, 0.1) for object_o in objects]
+    y_pred = [prediction['pred'] for prediction in predictions]
+    scores = [prediction['score'] for prediction in predictions]
     y_test, y_pred = np.array(y_test), np.array(y_pred)
     not_classified = np.sum(y_pred == -1)
     y_pred[y_pred == -1] = 0
-    return y_test, y_pred, not_classified
+    return y_test, y_pred, scores, not_classified
 
 
 if __name__ == '__main__':
--- src/posneg_rule_gen/posneg_rule_generation.py
+++ src/posneg_rule_gen/posneg_rule_generation.py
@@ -1,26 +1,35 @@
 import math
 from util import util
-import timeit
 
 
-def ponerg(itemset, classes, class_supp_count_dict, min_conf, transactions_df):
+def ponerg(itemset, classes, class_supp_count_dict, transactions_df):
     rules = []
     for c in classes:
-        i_and_c_supp_count = util.get_item_support_count_df(itemset | c, transactions_df)
-        i_supp_count = util.get_item_support_count_df(itemset, transactions_df)
-        lift = get_lift(i_and_c_supp_count, i_supp_count, class_supp_count_dict[c], len(transactions_df))
-        # r = correlation(itemset, c, transactions_df, i_supp_count, i_and_c_supp_count,
-        #                 class_supp_count_dict[c])
         c_str, = c
-        if lift > 1:
-            i_and_not_c_supp_count = util.get_support_count_i_and_not_c(itemset, c_str, transactions_df)
-            not_c_supp_count = util.get_item_support_count_df(c, transactions_df, negated=True)
-            # if (conf := confidence_selection(i_and_c_supp_count, i_supp_count, class_supp_count_dict[c],
-            #                                  i_and_not_c_supp_count, not_c_supp_count)) >= min_conf:
-            if (conf := confidence(i_and_c_supp_count, i_supp_count)) >= min_conf:
-                rules.append({'antecedent': itemset, 'consequent': c_str, 'confidence': conf})
-
+        cls = util.get_item_support_count_df(itemset | c, transactions_df) / class_supp_count_dict[c]
+        p = util.get_item_support_count_df(itemset, transactions_df) / len(transactions_df)
+        if (conf := cls / p) > 1:
+            rules.append({'antecedent': itemset, 'consequent': c_str, 'confidence': conf})
+            break
+        elif conf == 1:
+            break
     return rules
+    # for c in classes:
+    #     i_and_c_supp_count = util.get_item_support_count_df(itemset | c, transactions_df)
+    #     i_supp_count = util.get_item_support_count_df(itemset, transactions_df)
+    #     lift = get_lift(i_and_c_supp_count, i_supp_count, class_supp_count_dict[c], len(transactions_df))
+    #     # r = correlation(itemset, c, transactions_df, i_supp_count, i_and_c_supp_count,
+    #     #                 class_supp_count_dict[c])
+    #     c_str, = c
+    #     if lift > 1:
+    #         i_and_not_c_supp_count = util.get_support_count_i_and_not_c(itemset, c_str, transactions_df)
+    #         not_c_supp_count = util.get_item_support_count_df(c, transactions_df, negated=True)
+    #         # if (conf := confidence_selection(i_and_c_supp_count, i_supp_count, class_supp_count_dict[c],
+    #         #                                  i_and_not_c_supp_count, not_c_supp_count)) >= min_conf:
+    #         if (conf := confidence(i_and_c_supp_count, i_supp_count)) >= min_conf:
+    #             rules.append({'antecedent': itemset, 'consequent': c_str, 'confidence': conf})
+    #
+    # return rules
 
 
 def get_lift(i_and_c_supp_count, i_supp_count, class_supp_count, N):
