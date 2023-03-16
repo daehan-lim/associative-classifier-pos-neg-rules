92c2c64498951dcbcb219db9b2c8a15486fded38
Daehan Lim <penjan.eng@gmail.com>
2023-03-16 15:22:01 +0900

    Remove unnecessary get_lift comment since prof's cls / p is a quicker way to calculate it

--- src/posneg_rule_gen/posneg_rule_generation.py
+++ src/posneg_rule_gen/posneg_rule_generation.py
@@ -6,30 +6,23 @@ def ponerg(itemset, classes, class_supp_count_dict, transactions_df):
     rules = []
     for c in classes:
         c_str, = c
+        # r = correlation(itemset, c, transactions_df, i_supp_count, i_and_c_supp_count,
+        #                 class_supp_count_dict[c])
         cls = util.get_item_support_count_df(itemset | c, transactions_df) / class_supp_count_dict[c]
         p = util.get_item_support_count_df(itemset, transactions_df) / len(transactions_df)
-        if (conf := cls / p) > 1:
+        if (conf := cls / p) > 1:  # same as get_lift
             rules.append({'antecedent': itemset, 'consequent': c_str, 'confidence': conf})
             break
         elif conf == 1:
             break
     return rules
-    # for c in classes:
-    #     i_and_c_supp_count = util.get_item_support_count_df(itemset | c, transactions_df)
-    #     i_supp_count = util.get_item_support_count_df(itemset, transactions_df)
-    #     lift = get_lift(i_and_c_supp_count, i_supp_count, class_supp_count_dict[c], len(transactions_df))
-    #     # r = correlation(itemset, c, transactions_df, i_supp_count, i_and_c_supp_count,
-    #     #                 class_supp_count_dict[c])
-    #     c_str, = c
-    #     if lift > 1:
-    #         i_and_not_c_supp_count = util.get_support_count_i_and_not_c(itemset, c_str, transactions_df)
-    #         not_c_supp_count = util.get_item_support_count_df(c, transactions_df, negated=True)
-    #         # if (conf := confidence_selection(i_and_c_supp_count, i_supp_count, class_supp_count_dict[c],
-    #         #                                  i_and_not_c_supp_count, not_c_supp_count)) >= min_conf:
-    #         if (conf := confidence(i_and_c_supp_count, i_supp_count)) >= min_conf:
-    #             rules.append({'antecedent': itemset, 'consequent': c_str, 'confidence': conf})
-    #
-    # return rules
+    # if lift > 1:
+    #     i_and_not_c_supp_count = util.get_support_count_i_and_not_c(itemset, c_str, transactions_df)
+    #     not_c_supp_count = util.get_item_support_count_df(c, transactions_df, negated=True)
+    #     # if (conf := confidence_selection(i_and_c_supp_count, i_supp_count, class_supp_count_dict[c],
+    #     #                                  i_and_not_c_supp_count, not_c_supp_count)) >= min_conf:
+    #     if (conf := confidence(i_and_c_supp_count, i_supp_count)) >= min_conf:
+    #         rules.append({'antecedent': itemset, 'consequent': c_str, 'confidence': conf})
 
 
 def get_lift(i_and_c_supp_count, i_supp_count, class_supp_count, N):
