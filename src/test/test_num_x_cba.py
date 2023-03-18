9cbdc2f3c94c8f8b2eb8ac70f9287dc0a1a57f57
Daehan Lim <penjan.eng@gmail.com>
2023-03-18 22:42:40 +0900

    Stop splitting by class in test_num_x_cba.py

--- src/test/test_num_x_cba.py
+++ src/test/test_num_x_cba.py
@@ -8,24 +8,12 @@ from pathlib import Path
 with open('../../data/dataset.csv', 'r') as file:
     dataset = [list(filter(None, row)) for row in csv.reader(file)]
 
-transactions_df = util.convert_trans_to_df(dataset)
 random.seed(0)
-transactions_0 = pd.DataFrame(transactions_df[transactions_df['0']].reset_index(drop=True))
-transactions_1 = pd.DataFrame(
-    transactions_df[transactions_df['1']].reset_index(drop=True))
-
-indices = list(range(0, len(transactions_0)))
+transactions_df = util.convert_trans_to_df(dataset)
+indices = list(range(0, len(dataset)))
 random.shuffle(indices)
-test_set_0 = transactions_0.iloc[indices[:417], :].reset_index(drop=True)
-training_set_0 = transactions_0.iloc[indices[417:], :].reset_index(drop=True)
-
-indices = list(range(0, len(transactions_1)))
-random.shuffle(indices)
-test_set_1 = transactions_1.iloc[indices[:43], :].reset_index(drop=True)
-training_set_1 = transactions_1.iloc[indices[43:], :].reset_index(drop=True)
-
-training_set = pd.concat([training_set_0, training_set_1])
-test_set = pd.concat([test_set_0, test_set_1])
+training_set = transactions_df.iloc[indices[460:], :].reset_index(drop=True)
+test_set = transactions_df.iloc[indices[:460], :].reset_index(drop=True)
 
 arr = np.arange(training_set.values.shape[1])
 new_column_names = {col: str(i - 1) for i, col in enumerate(training_set.columns)}
