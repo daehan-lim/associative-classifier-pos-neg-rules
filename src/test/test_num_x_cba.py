e268fa3f514a4e68e21ff2839d9e541b6bcaa963
Daehan Lim <penjan.eng@gmail.com>
2023-04-06 14:14:26 +0900

    Create .num for cpar using whole dataset

--- src/test/test_num_x_cba.py
+++ src/test/test_num_x_cba.py
@@ -15,57 +15,45 @@ for seed in range(1, 10):
     transactions_df = util.convert_trans_to_df(dataset)
     indices = list(range(0, len(dataset)))
     random.shuffle(indices)
-    training_set = transactions_df.iloc[indices[460:], :].reset_index(drop=True)
+    # training_set = transactions_df.iloc[indices[460:], :].reset_index(drop=True)
     test_set = transactions_df.iloc[indices[:460], :].reset_index(drop=True)
 
-    arr = np.arange(training_set.values.shape[1])
-    new_column_names = {col: str(i - 1) for i, col in enumerate(training_set.columns)}
+    arr = np.arange(transactions_df.values.shape[1])
+    new_column_names = {col: str(i - 1) for i, col in enumerate(transactions_df.columns)}
     new_column_names['0'] = 148
     new_column_names['1'] = 149
 
-    training_set.rename(columns=new_column_names, inplace=True)
-    test_set.rename(columns=new_column_names, inplace=True)
+    transactions_df.rename(columns=new_column_names, inplace=True)
+    transactions_df.rename(columns=new_column_names, inplace=True)
 
-    training_transactions = training_set.drop([148, 149], axis=1).apply(lambda row: row.index[row], axis=1).tolist()
-    training_transactions = pd.DataFrame(training_transactions)
-    training_transactions.fillna("", inplace=True)
+    transactions = transactions_df.drop([148, 149], axis=1).apply(lambda row: row.index[row], axis=1).tolist()
+    transactions = pd.DataFrame(transactions)
+    transactions.fillna("", inplace=True)
 
-    y_training = training_set.apply(lambda row: 148 if row[148] else 149, axis=1).tolist()
-    training_transactions['class'] = y_training
+    y = transactions_df.apply(lambda row: 148 if row[148] else 149, axis=1).tolist()
+    transactions['class'] = y
 
-    test_transactions = test_set.drop([148, 149], axis=1).apply(lambda row: row.index[row], axis=1).tolist()
-    test_transactions = pd.DataFrame(test_transactions)
-    test_transactions.fillna("", inplace=True)
-
-    y_test = test_set.apply(lambda row: 148 if row[148] else 149, axis=1).tolist()
-    test_transactions['class'] = y_test
-
-    training_filename = '../../data/training_cba' + str(seed)
-    filepath = Path(training_filename + '.csv')
+    filename = '../../data/dataset_cpar' + str(seed)
+    filepath = Path(filename + '.csv')
     filepath.parent.mkdir(parents=True, exist_ok=True)
-    training_transactions.to_csv(filepath, index=False, header=False)
+    transactions.to_csv(filepath, index=False, header=False)
 
-    test_filename = '../../data/test_cba' + str(seed)
-    filepath = Path(test_filename + '.csv')
-    filepath.parent.mkdir(parents=True, exist_ok=True)
-    test_transactions.to_csv(filepath, index=False, header=False)
-
-    training_filename = '../../data/training_cba' + str(seed)
-    text = open(training_filename + '.csv', "r")
+    filename = '../../data/dataset_cpar' + str(seed)
+    text = open(filename + '.csv', "r")
     text = ''.join([i for i in text])
     text = re.sub(r',,+', ',', text)
     text = re.sub(r',', ' ', text)
-    x = open(training_filename + '.num', "w")
+    x = open(filename + '.num', "w")
     x.writelines(text)
     x.close()
 
-    test_filename = '../../data/test_cba' + str(seed)
-    text_test = open(test_filename + '.csv', "r")
-    text_test = ''.join([i for i in text_test])
-    text_test = re.sub(r',,+', ',', text_test)
-    text_test = re.sub(r',', ' ', text_test)
-    x_test = open(test_filename + '.num', "w")
-    x_test.writelines(text_test)
-    x_test.close()
+    # test_filename = '../../data/test_cba' + str(seed)
+    # text_test = open(test_filename + '.csv', "r")
+    # text_test = ''.join([i for i in text_test])
+    # text_test = re.sub(r',,+', ',', text_test)
+    # text_test = re.sub(r',', ' ', text_test)
+    # x_test = open(test_filename + '.num', "w")
+    # x_test.writelines(text_test)
+    # x_test.close()
 
     break
